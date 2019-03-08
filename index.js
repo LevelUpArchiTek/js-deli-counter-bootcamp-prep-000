87c11a5648f660e6d2cdc277f5b4a816f106447d
TK <zeropercent@hotmail.com>
2019-03-08 02:03:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 var katzDeliLine = []
 
 function takeANumber(position, name){
-  return `Welcome, ${name}. You are number ${position} in line.`
+  position.push(name)
+  return `Welcome, ${name}. You are number ${position.length} in line.`
 }
 
+
 function nowServing(katzDeliLine){
-  if (null === katzDeliLine){
-    return "There is nobody waiting to be served!"
-  } Else {
+  if (!katzDeliLine.length){
+    return `There is nobody waiting to be served!`
+  } else {
     var firstPerson = []
     firstPerson = katzDeliLine[0]
     katzDeliLine.shift()
-    return katzDeliLine
+    return `Currently serving ${firstPerson}.`
+  }
+}
+
+function currentLine(katzDeliLine){
+  var lineList
+  var j = 0
+  var i = 0
+  if (!katzDeliLine.length){
+    return `The line is currently empty.`
+  } else {
+    // for (var i = 0; i<katzDeliLine.length; i++){
+    //   var lineList
+    //   var j = i + 1
+    //   lineList = parseInt(j) + ". " + katzDeliLine[i]
+    // }\
+
+    do{
+        j = i + 1
+        lineList = parseInt(j) + ". " + katzDeliLine[i] + ", "
+        i++
+    }while(i < katzDeliLine.length)
+
+
+    return `"The line is currently: " + ${lineList}`
   }
 }
